e367715db472197fac859cf3b54e1df38b633750
Izaz Sarbudeen M <88654250+Lonelysing@users.noreply.github.com>
2022-02-13 20:21:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,34 +19,3 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
-මම Permanent Files Store Bot 🤖 කෙනෙක්!
-මට ඕනේ files එවන්න මන් ඒවා මගේ database එකට දා ගන්නම් 😏. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
-
-🤖 **මගේ නම:** [Files Store Bot](https://t.me/{BOT_USERNAME})
-
-📝 **මාව හැදුවේ :** [Python3](https://www.python.org)
-
-📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
-
-📡 **මන් ජිවත් වෙන්නේ Heroku server එකේ:** [Heroku](https://heroku.com)
-
-🧑🏻‍💻 **මාව හැදුවේ නම කියන්න අකමැති කෙනෙක්:** @senuinfinity
-
-👥 **උදව් ඕනෙද මේ group එකෙන් උදව් ගන්න:** [Senu Infinity Support](https://t.me/senuinfinitygroup)
-
-📢 **මගේ update channel එක:** [Senu Infinity](https://t.me/senuinfinity)
-"""
-	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @senuinfinity
-
-මාව හදපු කෙනා තාම මන් ඉස්කෝලේ යනවා . පුලුවන්නම් Donate කරාන්න මේ දේවල් පවත්වාගෙන යන්න.
-
-📌ඔන්න කිව්වා මගේ database එකට නරක ඒවා දැම්මොත් එවෙලේම remove කරනවා
-
-[Donate Now](Cooming Soon) (PayPal)
-"""
-	HOME_TEXT = """
-Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
-
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
-"""
